9b11f8c7df95bcde0604c48ded2dbbd3d3375c32
Baran <barandasdemir.bd@gmail.com>
2025-08-11 18:52:46 +0300

    temaEdit

--- Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -8,5 +8,55 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
+        public PartialViewResult _NavbarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _NavbarNotificationPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _NavbarLanguagePartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _NavbarProfilePartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _NavbarMessagePartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _SidebarPartPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _SidebarLogoPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _SidebarProfilePartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
