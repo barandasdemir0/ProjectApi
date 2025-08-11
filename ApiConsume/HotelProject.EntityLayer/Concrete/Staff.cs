51ee251e33f4a66bcc97230fb6a35f2b048d4246
Baran <barandasdemir.bd@gmail.com>
2025-08-11 09:50:03 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Staff //ekibimiz
+    {
+        public int StaffID { get; set; }
+        public string? StaffName { get; set; }
+        public string? StaffTitle { get; set; }
+        public string? StaffSocialMedia1 { get; set; }
+        public string? StaffSocialMedia2 { get; set; }
+        public string? StaffSocialMedia3 { get; set; }
+    }
+}
