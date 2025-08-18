ede8cfba335ab4d12da6a98afb94b375c32f9f06
Baran <barandasdemir.bd@gmail.com>
2025-08-18 13:57:48 +0300

    ımage yapılacak

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/ImageDto/FileImage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.ImageDto
+{
+    public class FileImage
+    {
+        public IFormFile? FileName { get; set; }
+    }
+}
