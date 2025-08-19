b54e763420d30deb7378d2f2d0777b9fc06fe7a3
Baran <barandasdemir.bd@gmail.com>
2025-08-19 19:56:38 +0300

    rolesChange

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController.cs
@@ -0,0 +1,66 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoleAssignController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleAssignController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RoleAssignInsert(int id)
+        {
+            var values = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["userid"] = values.Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(values);
+
+
+            List<RoleAssignViewModel> roleAssigns = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel roleAssign = new RoleAssignViewModel();
+                roleAssign.RoleId = item.Id;
+                roleAssign.RoleName = item.Name;
+                roleAssign.RoleExist = userRoles.Contains(item.Name);
+                roleAssigns.Add(roleAssign);
+            }
+            return View(roleAssigns);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoleAssignInsert(List<RoleAssignViewModel> role)
+        {
+            var userid = (int)TempData["userid"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach (var item in role)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
